87be9f913df1ac6dac79cf247ea4ac51c0114c68
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2026-02-08 15:18:43 -0500

    adding the form to the homepage and loading each note too

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-function HomePage() {
+import NoteForm from '@/components/NoteForm';
+
+async function loadNotes() {
+  const res = await fetch('http://localhost:3000/api/notes');
+  const data = await res.json();
+  return data;
+}
+
+async function HomePage() {
+  const notes = await loadNotes();
   return (
     <div>
-      <h1>Home Page</h1>
+      <NoteForm />
+      <ul>
+        {notes.map((note: any) => (
+          <li key={note.id}>{note.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/components/NoteForm.tsx
@@ -0,0 +1,26 @@
+function NoteForm() {
+    return (
+        <form>
+            <input 
+                type="text" 
+                name="title" 
+                placeholder="Title" 
+                autoFocus  
+                className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+            />
+            <textarea 
+                name="content" 
+                placeholder="Content" 
+                className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+            ></textarea>
+            <button 
+                type="submit"
+                className="px-5 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"
+            >
+                Add Note
+            </button>
+        </form>
+    );
+}
+
+export default NoteForm;
